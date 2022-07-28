5e6bb9a3c7ea9d9313e0b99629b921f97c7647ab
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-07-28 16:50:11 +0200

    added The Main content and BaseCard component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="base-card-container">
+        <div id="card" v-for="comic in comics" :key="comic.series">
+            <img :src="comic.thumb" :alt="comic.series">
+            <p>{{comic.series}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseCard',
+    props: {
+        comics: Array
+    }
+}
+</script>
+
+<style>
+    #card{
+        width: calc(100% / 6);
+        height: 300px;
+        padding: 40px;
+    }
+    #card p{
+        text-align: center;
+        text-transform: uppercase;
+    }
+    #card img{
+        max-width: 100%;
+        height: auto;
+    }
+
+    #base-card-container{
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+</style>
